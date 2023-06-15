795d8352e5f324f55c6926a1be64291ab89bb3a8
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-15 19:03:22 +0200

    feat: delete.js solve

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+
+import filesFolder from '../variables.js';
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemove = `${filesFolder}/fileToRemove.txt`;
+
+    fs.access(fileToRemove, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+        fs.unlink(fileToRemove, (err) => {
+            if (err) throw err;
+            console.log('FS operation succeed!');
+        });
+    });
 };
 
 await remove();
