7297e0884ffdef9434fc30644b8f22bb343659b6
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-15 19:35:53 +0200

    feat: read.js solve

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+
+import filesFolder from '../variables.js';
+
 const read = async () => {
-    // Write your code here 
+    const fileToRead = `${filesFolder}/fileToRead.txt`;
+    fs.readFile(fileToRead, 'utf-8', (err, data) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+        console.log(data);
+    })
 };
 
 await read();
