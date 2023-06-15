df5a75f86dc362ea0b84c7ed05ae5dce1a618781
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-15 18:54:44 +0200

    refactor: add semicolons

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -3,21 +3,21 @@ import fs from 'fs';
 import filesFolder from '../variables.js';
 
 const rename = async () => {
-    const oldFile = `${filesFolder}/wrongFilename.txt`
-    const newFile = `${filesFolder}/properFilename.md`
+    const oldFile = `${filesFolder}/wrongFilename.txt`;
+    const newFile = `${filesFolder}/properFilename.md`;
 
     fs.access(oldFile, (err) => {
         const oldFileErr = err;
 
         fs.access(newFile, (err) => {
             if (oldFileErr || !err) {
-                throw new Error('FS operation failed')
+                throw new Error('FS operation failed');
             }
             fs.rename(oldFile, newFile, (err) => {
                 if (err) throw err;
                 console.log('FS operation succeed!');
-            })
-        })
+            });
+        });
     });
 };
 
