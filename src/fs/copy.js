fa7b98a1fe0dd3eef3f005082b575453b7aea22e
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-15 18:29:12 +0200

    feat: copy.js solve

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,7 +1,24 @@
 import fs from 'fs';
 
+import filesFolder from '../variables.js';
+
 const copy = async () => {
-    // Write your code here
+    fs.access(filesFolder, (err) => {
+        const filesErr = err;
+        const filesCopy = `${filesFolder}_copy`;
+
+        fs.access(filesCopy, (err) => {
+            if (filesErr || !err) {
+                throw new Error('FS operation failed');
+            }
+            else {
+                fs.cp(filesFolder, `./${filesCopy}`, { recursive: true }, (err) => {
+                    if (err) throw err;
+                    console.log('FS operation succeed!');
+                });
+            }
+        });
+    });
 };
 
 await copy();
