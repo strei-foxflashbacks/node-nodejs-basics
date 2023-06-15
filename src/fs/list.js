05f4a8f7ed13f300daba8e0c5b6b670884433faf
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-15 19:30:13 +0200

    feat: list.js solve

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+
+import filesFolder from '../variables.js';
+
 const list = async () => {
-    // Write your code here 
+    fs.access(filesFolder, (err) => {
+        fs.readdir(filesFolder, (err, files) => {
+            if (err) {
+                throw new Error('FS operation failed');
+            }
+            console.log(files);
+        })
+    });
 };
 
 await list();
