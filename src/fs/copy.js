f5d349fc82fcaa353f91a93b019bd3c77a2d1714
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-16 00:03:18 +0200

    fix: rework due to npm functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,18 +1,25 @@
 import fs from 'fs';
+import path from 'path';
 
-import filesFolder from '../variables.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filesFolder = path.join(__dirname, 'files');
 
 const copy = async () => {
     fs.access(filesFolder, (err) => {
         const filesErr = err;
-        const filesCopy = `${filesFolder}_copy`;
+        const filesCopy = path.join('src/fs/', 'files_copy');
 
         fs.access(filesCopy, (err) => {
             if (filesErr || !err) {
                 throw new Error('FS operation failed');
             }
             else {
-                fs.cp(filesFolder, `./${filesCopy}`, { recursive: true }, (err) => {
+                fs.cp(filesFolder, filesCopy, { recursive: true }, (err) => {
                     if (err) throw err;
                     console.log('FS operation succeed!');
                 });
--- src/variables.js
+++
@@ -1,4 +0,0 @@
-import path from 'path';
-
-const filesFolder = path.basename('./files');
-export default filesFolder;
