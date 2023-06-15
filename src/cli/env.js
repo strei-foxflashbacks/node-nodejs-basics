0cc3636371c8d29e8759e3791c194d555e8307a9
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-16 01:07:46 +0200

    feat: env.js solve

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here
+    const rssArr = Object.entries(env).filter(([key, value]) => key.match(/RSS_/gi))
+    const output = rssArr.map((elem) => elem.join('=')).join('; ')
+    console.log(output)
 };
 
 parseEnv();
