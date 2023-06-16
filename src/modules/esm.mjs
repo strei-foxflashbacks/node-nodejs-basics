c453a1537a9c803502a67b3d53fbf7c7839195f0
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-16 16:56:19 +0200

    feat: esm.js solve

---
+++ src/modules/esm.mjs
@@ -0,0 +1,45 @@
+import path from 'path';
+import { release, version } from 'os';
+import { createServer } from 'node:http';
+import './files/c.js';
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const random = Math.random();
+
+export let unknownObject;
+
+if (random > 0.5) {
+    unknownObject = require("./files/a.json");
+} else {
+    unknownObject = require("./files/b.json");
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServer((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export default myServer;
+
