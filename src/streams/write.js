c5289b0517b43a05865cac0f860787f07aa4a674
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-16 20:06:51 +0200

    feat: write.js solve

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import { stdin, stdout } from 'node:process';
+import fs from 'fs';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filesFolder = path.join(__dirname, 'files');
+
 const write = async () => {
-    // Write your code here 
+    const fileToWrite = `${filesFolder}/fileToWrite.txt`;
+    const output = fs.createWriteStream(fileToWrite);
+    stdout.write(
+        'Entered writable stream. In order to exit, use Ctrl+C combination.\n'
+    );
+    stdin.on('data', (chunk) => {
+        output.write(chunk.toString())
+    });
 };
 
 await write();
