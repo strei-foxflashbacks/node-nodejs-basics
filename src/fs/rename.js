eebe6ec1e6e689d7246c2ae9c3de70362f6ee2e0
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-19 14:45:52 +0200

    feat: rename.js solve

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+
+import filesFolder from '../variables.js';
+
 const rename = async () => {
-    // Write your code here 
+    const oldFile = `${filesFolder}/wrongFilename.txt`
+    const newFile = `${filesFolder}/properFilename.md`
+
+    fs.access(oldFile, (err) => {
+        const oldFileErr = err;
+
+        fs.access(newFile, (err) => {
+            if (oldFileErr || !err) {
+                throw new Error('FS operation failed')
+            }
+            fs.rename(oldFile, newFile, (err) => {
+                if (err) throw err;
+                console.log('FS operation succeed!');
+            })
+        })
+    });
 };
 
 await rename();
