3af7655dd7b312635a49531c7e6149ae103a535d
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-19 14:45:52 +0200

    feat: create.js solve

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import path from 'path'
+import fs from 'fs'
+
+const files = path.basename('./files')
+
 const create = async () => {
-    // Write your code here 
+    const fresh = `${files}/fresh.txt`
+    fs.access(fresh, (err) => {
+        if (err) {
+            fs.appendFile(fresh, 'I am fresh and young', (err) => {
+                if (err) throw err;
+                console.log('FS operation succeed!')
+            })
+        }
+        else {
+            throw new Error('FS operation failed')
+        }
+    })
 };
 
 await create();
