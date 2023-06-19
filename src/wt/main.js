96af59104579a5e7f4f4a1a53bcd22029b67761b
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-19 13:14:13 +0200

    refactor: move to separate branch

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,17 @@
+import { Worker } from 'node:worker_threads';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToWorker = path.join(__dirname, 'worker.js')
+
+const worker = new Worker(pathToWorker, {
+    workerData: 'kekus!'
+});
+
 const performCalculations = async () => {
     // Write your code here
 };
