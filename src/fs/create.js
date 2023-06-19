93ad8a08705818eb30c6b5e1f5b48ed7890876e5
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-19 14:45:52 +0200

    fix: variables file & better alignment

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,19 +1,18 @@
-import path from 'path'
 import fs from 'fs'
 
-const files = path.basename('./files')
+import files from '../variables.js'
 
 const create = async () => {
     const fresh = `${files}/fresh.txt`
     fs.access(fresh, (err) => {
-        if (err) {
+        if (!err) {
+            throw new Error('FS operation failed')
+        }
+        else {
             fs.appendFile(fresh, 'I am fresh and young', (err) => {
                 if (err) throw err;
                 console.log('FS operation succeed!')
             })
-        }
-        else {
-            throw new Error('FS operation failed')
         }
     })
 };
