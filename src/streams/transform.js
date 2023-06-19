a4f60a8ce245346c5ed685a0525ca610301bd9df
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-19 14:45:52 +0200

    feat: transform.js solve

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+
+const reversed = new Transform({
+  transform(chunk, encoding, callback) {
+    const reversedData = chunk.toString()
+                              .split('')
+                              .reverse()
+                              .join('');
+    this.push(reversedData)
+    callback();
+  }
+})
+
 const transform = async () => {
-    // Write your code here 
+  stdin.pipe(reversed).pipe(stdout);
 };
 
 await transform();
