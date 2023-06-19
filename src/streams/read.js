2cf6da62882812ca4e01d8fa3db302bfede29a57
ssekridov@gmail.com <ssekridov@gmail.com>
2023-06-19 14:45:52 +0200

    feat: read.js solve

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filesFolder = path.join(__dirname, 'files');
+
 const read = async () => {
-    // Write your code here 
+    const fileToRead = `${filesFolder}/fileToRead.txt`;
+    const data = fs.createReadStream(fileToRead, 'utf-8');
+    data.on('data', (chunk) => {
+        console.log(chunk)
+    })
 };
 
 await read();
